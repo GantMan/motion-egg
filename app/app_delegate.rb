b701a14bfa0a95c88aa4ab21fd761e2166f58101
Gant <GantMan@gmail.com>
2014-04-25 12:26:35 -0500

    Konami code detection

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,4 +1,13 @@
 class AppDelegate
+  TOUCHES = 1
+  KONAMI = [
+    UISwipeGestureRecognizerDirectionUp, UISwipeGestureRecognizerDirectionUp, 
+    UISwipeGestureRecognizerDirectionDown, UISwipeGestureRecognizerDirectionDown, 
+    UISwipeGestureRecognizerDirectionLeft, UISwipeGestureRecognizerDirectionRight,
+    UISwipeGestureRecognizerDirectionLeft, UISwipeGestureRecognizerDirectionRight
+  ]
+  attr_accessor :current_code
+
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = RootController.new
@@ -7,25 +16,25 @@ class AppDelegate
     #left
     @swipe_gesture_left = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_left.direction = UISwipeGestureRecognizerDirectionLeft 
-    @swipe_gesture_left.numberOfTouchesRequired = 1
+    @swipe_gesture_left.numberOfTouchesRequired = TOUCHES
     @window.addGestureRecognizer(@swipe_gesture_left)
 
     #right
     @swipe_gesture_right = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_right.direction = UISwipeGestureRecognizerDirectionRight
-    @swipe_gesture_right.numberOfTouchesRequired = 1
+    @swipe_gesture_right.numberOfTouchesRequired = TOUCHES
     @window.addGestureRecognizer(@swipe_gesture_right)
 
     #up
     @swipe_gesture_up = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_up.direction = UISwipeGestureRecognizerDirectionUp
-    @swipe_gesture_up.numberOfTouchesRequired = 1
+    @swipe_gesture_up.numberOfTouchesRequired = TOUCHES
     @window.addGestureRecognizer(@swipe_gesture_up)    
 
     #down
     @swipe_gesture_down = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_down.direction = UISwipeGestureRecognizerDirectionDown
-    @swipe_gesture_down.numberOfTouchesRequired = 1
+    @swipe_gesture_down.numberOfTouchesRequired = TOUCHES
     @window.addGestureRecognizer(@swipe_gesture_down)
 
     true
@@ -33,6 +42,14 @@ class AppDelegate
 
   # each recognizer should have a direction, but you can send them all to the same function!
   def handle_swipe(sender)
+    @current_code ||= Array.new
+    @current_code.push(sender.direction)
+
+    # Only keep the last 
+    @current_code = @current_code[-8..-1] if @current_code.size > 8
+    p @current_code
+    p "************* OMG THE CODEZ! *************" if KONAMI == @current_code
+
     p "swipe handler called"
     if(sender.direction == UISwipeGestureRecognizerDirectionDown)
       p "Swiped down"
