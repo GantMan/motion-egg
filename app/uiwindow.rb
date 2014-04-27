d35503b0c4374a59d2821786dfb547f1706f1d86
Gant <GantMan@gmail.com>
2014-04-27 15:09:52 -0500

    Shows on Konami code

--- app/uiwindow.rb
+++ app/uiwindow.rb
@@ -40,23 +40,66 @@ class UIWindow
     # Only keep the last 
     @current_code = @current_code[-8..-1] if @current_code.size > 8
     p @current_code
-    p "************* OMG THE CODEZ! *************" if KONAMI == @current_code
-
-    p "swipe handler called"
-    if(sender.direction == UISwipeGestureRecognizerDirectionDown)
-      p "Swiped down"
-    end
-
-    if(sender.direction == UISwipeGestureRecognizerDirectionLeft)
-      p "Swiped left"
-    end
-
-    if(sender.direction == UISwipeGestureRecognizerDirectionRight)
-      p "Swiped right"
-    end
-
-    if(sender.direction == UISwipeGestureRecognizerDirectionUp)
-      p "Swiped up"
-    end
+    code_performed if KONAMI == @current_code
   end  
+
+  # Code performed!
+  def code_performed
+    p "************* OMG THE CODEZ! *************" 
+    insert_egg
+    show_egg
+  end
+
+  def show_egg(timing = 0.5)
+    # Animate the egg into the view:
+    UIView.animateWithDuration(timing,
+      delay:0.0,
+      options:UIViewAnimationOptionCurveLinear,
+      animations: lambda {
+        @egg_view.frame = showingFrame
+      },
+      completion:lambda {|finished|
+      }
+    )
+  end
+
+  def hide_egg(timing = 0.5)
+    # Animate the card out of the view:
+    UIView.animateWithDuration(timing,
+      delay:0.0,
+      options:UIViewAnimationOptionCurveLinear,
+      animations: lambda {
+        @egg_view.frame = hiddenFrame
+      },
+      completion:nil
+    )
+  end  
+
+  def insert_egg
+    @egg_view ||= begin
+      egg_view = UIView.new
+      egg_view.backgroundColor = UIColor.blackColor
+      #egg_view.addSubview()
+      egg_view.frame = hidden_frame
+      egg_view
+    end
+ 
+    # Add stuff to the egg
+    #@egg_view.addSubview(something_here)
+ 
+    # We want to add the view to the root view
+    UIApplication.sharedApplication.keyWindow.subviews.objectAtIndex(0).nextResponder.view.addSubview(@egg_view)
+  end  
+
+  def hidden_frame
+    [[0,UIScreen.mainScreen.bounds.size.height + egg_height],[UIScreen.mainScreen.bounds.size.width, egg_height]]
+  end
+
+  def showingFrame
+    [[0,UIScreen.mainScreen.bounds.size.height - egg_height],[UIScreen.mainScreen.bounds.size.width, egg_height]]
+  end  
+
+  def egg_height
+    200
+  end
 end
