2fa1214f5cbc49129cab79c8b5affb6f78f0ff3f
Gant <GantMan@gmail.com>
2014-04-28 23:03:19 -0500

    Updated example to explain

--- app/root_controller.rb
+++ app/root_controller.rb
@@ -1,7 +1,18 @@
 class RootController < UIViewController
 
   def viewDidLoad
-    view.backgroundColor = UIColor.grayColor
+    view.backgroundColor = UIColor.lightGrayColor
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Perform the Konami Code in Gestures \n
+                  UP, UP, \n
+                  DOWN, DOWN, \n
+                  LEFT, RIGHT, \n
+                  LEFT, RIGHT"
+    @label.numberOfLines = 0
+    @label.font = UIFont.systemFontOfSize(16)
+    @label.sizeToFit
+    @label.center = view.center
+    view.addSubview(@label)    
   end
 
 end
