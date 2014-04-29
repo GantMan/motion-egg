8528868f999af0a3ad5b495a6b17dde796658879
Gant <GantMan@gmail.com>
2014-04-29 14:43:57 -0500

    began moving configuration to separate class

---
+++ lib/motion_egg/egg.rb
@@ -0,0 +1,40 @@
+class Egg
+  attr_accessor :number_touches, :secret_code, :current_code
+
+  DEFAULT_TOUCHES = 1
+  KONAMI = [
+    UISwipeGestureRecognizerDirectionUp, UISwipeGestureRecognizerDirectionUp, 
+    UISwipeGestureRecognizerDirectionDown, UISwipeGestureRecognizerDirectionDown, 
+    UISwipeGestureRecognizerDirectionLeft, UISwipeGestureRecognizerDirectionRight,
+    UISwipeGestureRecognizerDirectionLeft, UISwipeGestureRecognizerDirectionRight
+  ]
+
+  def initialize 
+
+  end
+
+  def number_touches
+    @number_touches || DEFAULT_TOUCHES
+  end
+
+  def secret_code
+    @secret_code || KONAMI
+  end
+
+  def hidden_frame
+    [[0,UIScreen.mainScreen.bounds.size.height + egg_height],[egg_width, egg_height]]
+  end
+
+  def show_frame
+    [[0,UIScreen.mainScreen.bounds.size.height - egg_height],[egg_width, egg_height]]
+  end  
+
+  def egg_height
+    180
+  end
+
+  def egg_width
+    UIScreen.mainScreen.bounds.size.width
+  end
+
+end
--- lib/motion_egg/uiwindow.rb
+++ lib/motion_egg/uiwindow.rb
@@ -1,36 +1,33 @@
 class UIWindow
   attr_accessor :current_code
-  
-  TOUCHES = 1
-  KONAMI = [
-    UISwipeGestureRecognizerDirectionUp, UISwipeGestureRecognizerDirectionUp, 
-    UISwipeGestureRecognizerDirectionDown, UISwipeGestureRecognizerDirectionDown, 
-    UISwipeGestureRecognizerDirectionLeft, UISwipeGestureRecognizerDirectionRight,
-    UISwipeGestureRecognizerDirectionLeft, UISwipeGestureRecognizerDirectionRight
-  ]
+
   def add_egg
+
+    # Easter egg workhorse
+    @egg = Egg.new
+
     #left
     @swipe_gesture_left = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_left.direction = UISwipeGestureRecognizerDirectionLeft 
-    @swipe_gesture_left.numberOfTouchesRequired = TOUCHES
+    @swipe_gesture_left.numberOfTouchesRequired = @egg.number_touches
     self.addGestureRecognizer(@swipe_gesture_left)
 
     #right
     @swipe_gesture_right = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_right.direction = UISwipeGestureRecognizerDirectionRight
-    @swipe_gesture_right.numberOfTouchesRequired = TOUCHES
+    @swipe_gesture_right.numberOfTouchesRequired = @egg.number_touches
     self.addGestureRecognizer(@swipe_gesture_right)
 
     #up
     @swipe_gesture_up = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_up.direction = UISwipeGestureRecognizerDirectionUp
-    @swipe_gesture_up.numberOfTouchesRequired = TOUCHES
+    @swipe_gesture_up.numberOfTouchesRequired = @egg.number_touches
     self.addGestureRecognizer(@swipe_gesture_up)    
 
     #down
     @swipe_gesture_down = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handle_swipe:")
     @swipe_gesture_down.direction = UISwipeGestureRecognizerDirectionDown
-    @swipe_gesture_down.numberOfTouchesRequired = TOUCHES
+    @swipe_gesture_down.numberOfTouchesRequired = @egg.number_touches
     self.addGestureRecognizer(@swipe_gesture_down)  
   end
 
@@ -42,7 +39,7 @@ class UIWindow
     # Only keep the last 
     @current_code = @current_code[-8..-1] if @current_code.size > 8
     p @current_code
-    code_performed if KONAMI == @current_code
+    code_performed if @egg.secret_code == @current_code
   end  
 
   # Code performed!
@@ -59,7 +56,7 @@ class UIWindow
       delay:0.0,
       options:UIViewAnimationOptionCurveLinear,
       animations: lambda {
-        @egg_view.frame = showingFrame
+        @egg_view.frame = @egg.show_frame
       },
       completion:lambda {|finished|
       }
@@ -72,7 +69,7 @@ class UIWindow
       delay:0.0,
       options:UIViewAnimationOptionCurveLinear,
       animations: lambda {
-        @egg_view.frame = hidden_frame
+        @egg_view.frame = @egg.hidden_frame
       },
       completion:nil
     )
@@ -80,10 +77,9 @@ class UIWindow
 
   def insert_egg
     @egg_view ||= begin
-      egg_view = UIImageView.alloc.initWithFrame([[0,0],[egg_width, egg_height]]) #UIView.new
+      egg_view = UIImageView.alloc.initWithFrame(@egg.hidden_frame)
       egg_view.contentMode = UIViewContentModeScaleAspectFit
       egg_view.image = UIImage.imageNamed("toasty.png")
-      egg_view.frame = hidden_frame
       egg_view
     end
  
@@ -91,19 +87,4 @@ class UIWindow
     UIApplication.sharedApplication.keyWindow.subviews.objectAtIndex(0).nextResponder.view.addSubview(@egg_view)
   end  
 
-  def hidden_frame
-    [[0,UIScreen.mainScreen.bounds.size.height + egg_height],[egg_width, egg_height]]
-  end
-
-  def showingFrame
-    [[0,UIScreen.mainScreen.bounds.size.height - egg_height],[egg_width, egg_height]]
-  end  
-
-  def egg_height
-    180
-  end
-
-  def egg_width
-    UIScreen.mainScreen.bounds.size.width
-  end
 end
